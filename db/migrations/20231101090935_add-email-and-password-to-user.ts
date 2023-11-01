4d289ad99f21088c2644add602f9a8226a4bfe8d
Ramon Borges <ramonboorges@gmail.com>
2023-11-01 10:32:28 +0100

    fix: fix migration to make user email unique

--- db/migrations/20231101090935_add-email-and-password-to-user.ts
+++ db/migrations/20231101090935_add-email-and-password-to-user.ts
@@ -3,7 +3,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable('users', (table) => {
-        table.text("email").notNullable(),
+        table.text("email").notNullable().unique(),
         table.text("password").notNullable()
     })
 }
