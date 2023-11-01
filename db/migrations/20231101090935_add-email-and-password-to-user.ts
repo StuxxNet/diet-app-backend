d433f73c96beb559b513d534985b8400f583e37e
Ramon Borges <ramonboorges@gmail.com>
2023-11-01 10:21:01 +0100

    feat: Implemented basic login route

---
+++ db/migrations/20231101090935_add-email-and-password-to-user.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('users', (table) => {
+        table.text("email").notNullable(),
+        table.text("password").notNullable()
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.alterTable('users', (table) => {
+        table.dropColumns(...["email", "password"])
+    })
+}
+
--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,18 @@ import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import { mealsRoutes } from './routes/meals'
 import { usersRoutes } from './routes/users'
+import { loginRoutes } from './routes/login'
 
 export const app = fastify()
 
 app.register(cookie)
-app.register(mealsRoutes, {
-    prefix: 'meals'
+
+app.register(loginRoutes, {
+    prefix: 'login'
 })
 app.register(usersRoutes, {
     prefix: 'users'
+})
+app.register(mealsRoutes, {
+    prefix: 'meals'
 })
