dc8164a5a0ebcf7f23a4bb32a9736812bd9c8b6c
ramon.silveira-borges@telekom.com <ramon.silveira-borges@telekom.com>
2024-01-26 19:58:24 -0300

    feat: add test case to get a user

--- test/users.spec.ts
+++ test/users.spec.ts
@@ -28,5 +28,24 @@ describe('UsersRoute', () => {
 
       expect(response.statusCode).toEqual(201)
     })
+
+    it('should be able to get a created user', async () => {
+      const userCreationResponse = await request(app.server)
+        .post('/users')
+        .send({
+          name: 'Ramon Borges',
+          born_date: '07/04/1994',
+          email: 'ramonboorges@gmail.com',
+          password: 'abc1234',
+        })
+      const cookies = userCreationResponse.headers['set-cookie']
+
+      const response = await request(app.server)
+        .get('/users')
+        .set('Cookie', cookies)
+        .send()
+
+      expect(response.statusCode).toEqual(200)
+    })
   })
 })
